6b15b41fa56297fe96ea7a31d1547c4744591eb2
François Pluchino <francois.pluchino@klipper.dev>
2020-06-03 19:20:31 +0200

    Add php type hints and update phpdoc

--- DependencyInjection/KlipperSmsSenderExtension.php
+++ DependencyInjection/KlipperSmsSenderExtension.php
@@ -28,9 +28,6 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  */
 class KlipperSmsSenderExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
